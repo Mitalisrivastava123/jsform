31a09ab378e4404e102e5d755ede8eea46e15b2d
Mitalisri <mitalisrivastava@cedcoss.com>
2022-11-10 14:33:02 +0530

    code

--- src/script.js
+++ src/script.js
@@ -4,26 +4,43 @@ function func1()
     var x = document.getElementById("age").value;
     var y=document.getElementById("weight").value;
 
-    if(x<=7 && y<=20)
-    {
-   
-    document.getElementById("demo3").innerHTML="your weight is perfect";
+    if(x<=7&&x>=5)
+    { 
+    if(y<=20&&y>=15){
+        document.getElementById("demo3").innerHTML="your weight is perfect";
+    }else if(y<15){
+        document.getElementById("demo3").innerHTML="your weight is underweight";
+    }else if(y>20){
+        document.getElementById("demo3").innerHTML="your weight is overweight";
     }
-    else if(x<=8 && y<=25)
-    {
-        document.getElementById("demo3").innerHTML="you are underweight";
     }
-    else if(x<=15 && y<=30)
+    else if(x>=8&&x<=10  )
     {
-        document.getElementById("demo3").innerHTML="your weight is normal";
+        if(y>=21&&y<=25){
+            document.getElementById("demo3").innerHTML="your weight is perfect ";
+        }else if(y<21){
+            document.getElementById("demo3").innerHTML="your weight is underweight";
+        }else if(y>25){
+            document.getElementById("demo3").innerHTML="your weight is overweight";
+        }
+    }else if(x<=15&&x>=11 ){
+        if(y>=26&&y<=30){
+            document.getElementById("demo3").innerHTML="your weight is perfect ";
+        }else if(y<26){
+            document.getElementById("demo3").innerHTML="your weight is underweight";
+        }else if(y>30){
+            document.getElementById("demo3").innerHTML="your weight is overweight";
+        }
     }
-    else if(x<=20 && y<=40)
+    else if(x<=20&&x>=16 )
     {
-        document.getElementById("demo3").innerHTML="your weight is good";
+        if(y<=40&&y>=31){
+            document.getElementById("demo3").innerHTML="your weight is perfect ";
+        }else if(y<31){
+            document.getElementById("demo3").innerHTML="your weight is underweight";
+        }else if(y>40){
+            document.getElementById("demo3").innerHTML="your weight is overweight";
+        }
     }
-    else if(x<=30 && y<=60)
-    {
-        document.getElementById("demo3").innerHTML="you are overweight";
-    }
- 
+    
 }
